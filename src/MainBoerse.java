79dc129f7fa83cf5891baa5925d9b8afd744ea79
dynastyo <030tee@gmail.com>
2022-11-17 14:54:28 +0100

    day1

--- src/MainBoerse.java
+++ src/MainBoerse.java
@@ -9,6 +9,13 @@ public class MainBoerse {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        dummyDaten(150);
+
+        for (Fahrzeug fahrzeug: datenbank
+             ) {
+            System.out.println(fahrzeug.getClass().getName());
+        }
+
         hauptMenue();
     }
 
@@ -22,45 +29,51 @@ public class MainBoerse {
         System.out.println("0) Börse beenden");
         System.out.println("--------------------------------------------------------------------------------------");
         System.out.println("Bitte wählen:");
+        System.out.println("""
+                ------------------------------------------------------
+                Fahrzeugbörse        Hauptmenü        von: Till Kosiol
+                
+                """);
         String choice = sc.nextLine();
         switch (choice) {
-//            case "1" -> menuAddCar();
-//            case "2" -> menuPrintCarpark();
-//            case "3" -> menuSearchCar();
-//            case "4" -> menuDeleteCar();
-//            case "5" -> menuSortCarpark();
+//            case "1" -> anlegen();
+//            case "2" -> bearbeiten();
+//            case "3" -> suchen();
+//            case "4" -> löschen();
+//            case "0" -> beenden();
             default -> {
                 System.out.println("Falsche eingabe! Probiers nochmal!");
                 hauptMenue();
             }
         }
-
-//        datenbank.add(new Boot("1", "asd", "sdasd",23132131231232L , 3 ));
-        System.out.println(datenbank.get(1).getBaujahr());
+    }
+    
+    public static void anlegen(){
+        
     }
 
 
-    public void dummyDaten(int amountOfCars) {
+    public static void dummyDaten(int amountOfCars) {
         String[] marken = new String[]{"BMW", "Audi", "VW", "Opel", "Dacia", "Subaru", "Suzuki"};
         String[] modelle = new String[]{"500", "A7", "Corsa", "3", "Tiguan", "F240", "Diabolo"};
         String[] farben = new String[]{"Rot", "Gruen", "Blau", "Silber", "Regenbogen", "Glitzer", "Fantasie"};
-        String[] klassen = new String[]{"Pkw", "Lkw"};
-//        Class[] typen = new Class[]{Pkw, Lkw, Boot.class, Motorrad.class};
-
+       
         Random rand = new Random();
         for (int i = 0; i < amountOfCars; i++) {
             int marke = rand.nextInt(marken.length);
             int modell = rand.nextInt(modelle.length);
             int farbe = rand.nextInt(farben.length);
-            int klasse = rand.nextInt(klassen.length);
-            int typ = rand.nextInt(typen.length);
             double preis = Math.round(rand.nextDouble(500, 100000) * 100) / 100.00;
-            int baujahr = rand.nextInt(80, 270);
-
-            datenbank.add(new typen[typ]("1", "asd", "sdasd",23132131231232L , 3 ));
-            Class.forName(klassen[klasse]());
+            int baujahr = rand.nextInt(1900, 2022);
+            
+            int randKlasse = rand.nextInt(4);
+            switch (randKlasse) {
+                case 0 -> datenbank.add(new Pkw(marken[marke], modelle[modell], farben[farbe], baujahr, preis));
+                case 1 -> datenbank.add(new Lkw(marken[marke], modelle[modell], farben[farbe], baujahr, preis));
+                case 2 -> datenbank.add(new Boot(marken[marke], modelle[modell], farben[farbe], baujahr, preis));
+                case 3 -> datenbank.add(new Motorrad(marken[marke], modelle[modell], farben[farbe], baujahr, preis));
+            }
         }
     }
-
 }
 
